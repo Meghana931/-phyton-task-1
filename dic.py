82df6f3398a5419adf2d7af03333094802a7e5af
Meghana931 <86868270+Meghana931@users.noreply.github.com>
2021-12-31 23:19:31 +0530

    Add files via upload

---
+++ dic.py
@@ -0,0 +1,18 @@
+"""Write Python code to create a functioncalled most_frequent that takes a string 
+ and prints the letters in decreasing order of frequency. Use dictionaries.
+ """
+string = input("please enter desired string to analyse")
+
+
+def most_frequent(string):
+  freq_counts = {}
+  for char in string:
+    freq_counts.setdefault(char, 0)
+    freq_counts[char] += 1
+
+  sorted_freq_counts = sorted(freq_counts.items(), key=lambda x : x[1], reverse=True)
+  print("char, freq")
+  for char, count in sorted_freq_counts:
+    print(f"{char},{count}")
+
+most_frequent(string)
